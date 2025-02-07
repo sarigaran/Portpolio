3c553eb0b9ec23bc878e14e794322ebe70702671
sarigaran <arigaran.sundaram@gmail.com>
2025-02-07 11:47:51 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";import './App.css';
 import Applayout from './components/layout';
 
 function App() {
   return (
+
+    
     <div className="App">
-      <Applayout/>
+      <Router basename="/Portpolio">
+      <Routes>
+        <Route path="/" element={ <Applayout/>} />
+      </Routes>
+    </Router>
+     
     </div>
   );
 }
